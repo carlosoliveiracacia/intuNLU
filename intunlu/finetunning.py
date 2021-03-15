b7ec293fd0be979e2519df5d04e85ca5edcf22de
Carlos Oliveira <carlos.oliveira.cacia@gmail.com>
2021-03-15 17:09:34 -0600

    Dev/carlos/finetunning code (#1)
    * first functional solution
    
    * moved tokenization to the training runtime for faster computation
    
    * adding timing
    
    * sending tensors to device before calling model
    
    * fix
    
    * fixing timing
    
    * added logic to be able to train on a fixed subset of the original training set
    
    * added evaluation
    
    * sending model to device during evaluation
    
    * adding logging to file
    
    * replacing carriage return by spaces
    
    * logger for each run
    
    * doing only test evaluation
    
    * reduzing maximum input length (cuda is going out of memory)
    
    * truncating tokenization
    
    * fixing passing of argument ot model
    
    Co-authored-by: Carlos Oliveira <carlos_oliveira@intuit.com>

---
+++ intunlu/finetunning.py
@@ -0,0 +1,186 @@
+import logging
+import pytorch_lightning as pl
+import torch
+
+from torch.utils.data import DataLoader, Dataset, RandomSampler
+from transformers import T5ForConditionalGeneration
+
+
+class SummarizerModel(pl.LightningModule):
+    # Instantiate the model
+    def __init__(
+            self,
+            model_name,
+            tokenizer,
+            learning_rate=2e-5,
+            freeze_encoder=False,
+            freeze_embeds=False,
+            optimizer='Adam',
+            max_input_length=300
+    ):
+        super().__init__()
+        self.save_hyperparameters()
+        self.model = T5ForConditionalGeneration.from_pretrained(model_name)
+        self.learning_rate = learning_rate
+        self.tokenizer = tokenizer
+        self.optimizer = optimizer
+        self.max_input_length = max_input_length
+
+        if freeze_encoder:
+            self._freeze_params(self.model.get_encoder())
+
+        if freeze_embeds:
+            self._freeze_embeds()
+
+    @staticmethod
+    def _freeze_params(model):
+        ''' Function that takes a model as input (or part of a model) and freezes the layers for faster training
+          adapted from finetune.py '''
+        for layer in model.parameters():
+            layer.requires_grade = False
+
+    def _freeze_embeds(self):
+        ''' freeze the positional embedding parameters of the model; adapted from finetune.py '''
+        freeze_params(self.model.model.shared)
+        for d in [self.model.model.encoder, self.model.model.decoder]:
+            freeze_params(d.embed_positions)
+            freeze_params(d.embed_tokens)
+
+    # Do a forward pass through the model
+    def forward(self, input_ids, **kwargs):
+        return self.model(input_ids, **kwargs)
+
+    def configure_optimizers(self):
+        if self.optimizer == 'Adam':
+            optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
+        else:
+            raise ValueError(f'value of `optimizer` = {self.optimizer} not valid.')
+        return optimizer
+
+    def training_step(self, batch, batch_idx):
+
+        documents, summaries = batch
+
+        documents = self.tokenizer(
+            documents,
+            max_length=self.max_input_length,
+            padding='longest',
+            truncation=True,
+            return_tensors='pt'
+        )
+        summaries = self.tokenizer(
+            summaries,
+            max_length=self.max_input_length,
+            padding='longest',
+            truncation=True,
+            return_tensors='pt'
+        )
+
+        src_ids, src_mask, labels = documents['input_ids'], documents['attention_mask'], summaries['input_ids']
+
+        labels[labels[:, :] == self.tokenizer.pad_token_id] = -100
+
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        outputs = self(
+            input_ids=src_ids.to(device),
+            attention_mask=src_mask.to(device),
+            labels=labels.to(device)
+        )
+
+        loss = outputs.loss
+
+        return {'loss': loss}
+
+    def training_step_end(self, batch_parts):
+        train_loss = batch_parts['loss'].sum()
+        return {'loss': train_loss}
+
+    def training_epoch_end(self, training_step_outputs):
+        avg_loss = torch.stack([x['loss'] for x in training_step_outputs]).mean()
+        logging.info(f'Train loss: {avg_loss} (epoch {self.current_epoch})')
+        self.log('train_loss', avg_loss, on_step=False, on_epoch=True, prog_bar=False)
+
+    def validation_step(self, batch, batch_idx):
+
+        documents, summaries = batch
+
+        documents = self.tokenizer(
+            documents,
+            max_length=self.max_input_length,
+            padding='longest',
+            truncation=True,
+            return_tensors='pt'
+        )
+        summaries = self.tokenizer(
+            summaries,
+            max_length=self.max_input_length,
+            padding='longest',
+            truncation=True,
+            return_tensors='pt'
+        )
+
+        src_ids, src_mask, labels = documents['input_ids'], documents['attention_mask'], summaries['input_ids']
+
+        labels[labels[:, :] == self.tokenizer.pad_token_id] = -100
+
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        outputs = self(
+            input_ids=src_ids.to(device),
+            attention_mask=src_mask.to(device),
+            labels=labels.to(device)
+        )
+
+        loss = outputs.loss
+
+        return {'loss': loss}
+
+    def validation_epoch_end(self, validation_step_outputs):
+        avg_loss = torch.stack([x['loss'] for x in validation_step_outputs]).mean()
+        logging.info(f'Val loss: {avg_loss} (epoch {self.current_epoch})')
+        self.log('val_loss', avg_loss, on_step=False, on_epoch=True, prog_bar=False)
+
+
+class SummaryDataset(Dataset):
+
+    def __init__(self, data):
+        self.data = data
+
+    def __len__(self):
+        return len(self.data['document'])
+
+    def __getitem__(self, idx):
+        return self.data['document'][idx], self.data['summary'][idx]
+
+
+# Create a dataloading module as per the PyTorch Lightning Docs
+class SummaryDataModule(pl.LightningDataModule):
+    def __init__(
+            self,
+            train_dataset,
+            valid_dataset,
+            tokenizer,
+            batch_size,
+            max_num_samples=None
+    ):
+        super().__init__()
+        self.tokenizer = tokenizer
+        self.batch_size = batch_size
+        self.max_num_samples = max_num_samples
+        self.train = train_dataset
+        self.valid = valid_dataset
+
+    # Load the training, validation  sets in Pytorch Dataset objects
+    def train_dataloader(self):
+        dataset = SummaryDataset(self.train)
+        train_data = DataLoader(
+            dataset,
+            sampler=RandomSampler(dataset),
+            batch_size=self.batch_size,
+            drop_last=True
+        )
+        return train_data
+
+    def val_dataloader(self):
+        dataset = SummaryDataset(self.valid)
+        val_data = DataLoader(dataset, batch_size=self.batch_size, drop_last=True)
+        return val_data
---
+++ intunlu/train.py
@@ -0,0 +1,179 @@
+import logging
+import pickle
+import pytorch_lightning as pl
+import random
+import time
+import torch
+
+from datasets import load_dataset
+from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+from pytorch_lightning.loggers import TensorBoardLogger
+from rouge_score import rouge_scorer
+from transformers import T5Tokenizer
+
+from intunlu.finetunning import SummaryDataModule, SummarizerModel
+
+
+def train(
+        model_name='t5-small',
+        max_input_length=512,
+        batch_size=2,
+        n_max_epochs=10,
+        random_state=1234,
+        max_num_samples=2000
+):
+
+    setup_logger(random_state)
+
+    random.seed(1234)  # this is meant to be fixed so we can train on a fixed subset of the original training set
+    datasets = load_data(max_num_samples=max_num_samples)
+
+    pl.utilities.seed.seed_everything(random_state)
+
+    tokenizer = T5Tokenizer.from_pretrained(model_name)
+
+    data = SummaryDataModule(
+        datasets['train'],
+        datasets['validation'],
+        tokenizer,
+        batch_size,
+        max_num_samples=max_num_samples
+    )
+
+    model = SummarizerModel(
+        model_name=model_name,
+        tokenizer=tokenizer,
+        learning_rate=2e-5,
+        freeze_encoder=False,
+        freeze_embeds=False,
+        optimizer='Adam',
+        max_input_length=max_input_length
+    )
+
+    logger = TensorBoardLogger('logger', f'summarizer_{random_state}')
+
+    early_stop_callback = EarlyStopping(
+        monitor='val_loss',
+        min_delta=0.0,
+        patience=1,
+        verbose=False,
+        mode='min'
+    )
+
+    trainer = pl.Trainer(
+        gpus=torch.cuda.device_count(),
+        max_epochs=n_max_epochs,
+        min_epochs=1,
+        auto_lr_find=False,
+        accelerator='dp' if torch.cuda.device_count() > 1 else None,
+        logger = logger,
+        callbacks=[early_stop_callback]
+    )
+
+    logging.info('Starting training...')
+    s = time.time()
+    trainer.fit(model, data)
+    logging.info('Done with training.')
+    logging.info(f'(Took {time.time() - s} seconds.)')
+    trainer.save_checkpoint(f'summarizer_{random_state}')
+
+    logging.info('Starting evaluation...')
+    s = time.time()
+    results = {}
+    # results['valid'] = evaluate(model, datasets['validation'])
+    results['test'] = evaluate(model, datasets['test'], max_input_length)
+    for ds in results:
+        logging.info(f'Metrics for {ds} set:')
+        for m in ['rouge1', 'rouge2', 'rougeL']:
+            logging.info(f'Average {m} score: {sum(results[ds][m]) / len(results[ds][m])}')
+    logging.info('Done with evaluation.')
+    logging.info(f'(Took {time.time() - s} seconds.)')
+    with open(f'results_{random_state}.pkl', 'wb')as f:
+        pickle.dump(results, f)
+
+
+def load_data(max_num_samples=None):
+
+    # load datasets and trim them down as needed
+    datasets = {}
+    for ds in ['train', 'validation', 'test']:
+        dataset = load_dataset('xsum', split=ds)
+        N = len(dataset)
+        logging.info(f'{ds} dataset: initial # of samples is {N}.')
+        if max_num_samples is not None:
+            n = min(max_num_samples, N)
+        else:
+            n = N
+        logging.info(f'{ds} dataset: will effectively use {n} samples.')
+        dataset = list(zip(dataset['document'], dataset['summary']))
+        dataset = random.sample(dataset, n)
+        datasets[ds] = {
+            'document': ['summarize: ' + e[0].replace('\n', ' ') for e in dataset],
+            'summary': [e[1] for e in dataset]
+        }
+
+    return datasets
+
+def evaluate(model, dataset, max_input_length):
+
+    scorer = rouge_scorer.RougeScorer(['rouge1', 'rouge2', 'rougeL'], use_stemmer=False)
+
+    results = {
+        'reference_summary': [],
+        'predicted_summary': [],
+        'rouge1': [],
+        'rouge2': [],
+        'rougeL': []
+    }
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.model.to(device)
+    for i in range(len(dataset['document'])):
+        document = model.tokenizer(
+            'summarize: ' + dataset['document'][i],
+            max_length=max_input_length,
+            padding='longest',
+            truncation=True,
+            return_tensors='pt'
+        )
+        with torch.no_grad():
+            pred = model.model.generate(
+                input_ids=document['input_ids'].to(device),
+                attention_mask=document['attention_mask'].to(device),
+                use_cache=True,
+                decoder_start_token_id=model.tokenizer.pad_token_id,
+                num_beams=1,  # greedy search
+                max_length=max_input_length,
+                early_stopping=True
+            )
+            pred = model.tokenizer.convert_ids_to_tokens(pred[0], skip_special_tokens=True)
+            pred = model.tokenizer.convert_tokens_to_string(pred).replace(' . ', '. ')
+            if i < 2:
+                logging.info('The input document:')
+                logging.info(dataset['document'][i])
+                logging.info('The reference summary:')
+                logging.info(dataset['summary'][i])
+                logging.info('The predicted summary:')
+                logging.info(pred)
+        results['reference_summary'].append(dataset['summary'][i])
+        results['predicted_summary'].append(pred)
+        scores = scorer.score(results['reference_summary'][0], results['predicted_summary'][0])
+        results['rouge1'].append(scores['rouge1'][1])
+        results['rouge2'].append(scores['rouge2'][1])
+        results['rougeL'].append(scores['rougeL'][2])
+
+    return results
+
+def setup_logger(random_state):
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  [%(filename)s:%(lineno)s] [%(funcName)20s()] %(message)s",
+        handlers=[
+            logging.FileHandler(f"{random_state}.log"),
+            logging.StreamHandler()
+        ]
+    )
+
+if __name__ == '__main__':
+    train()
