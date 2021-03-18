eb72e2ade7839618db75f7609c965ed7ed72edd4
wyx <wyx@wyxs-MacBook-Pro.local>
2021-03-17 18:19:04 -0700

    max and mean score parameter

--- intunlu/generate_ensemble.py
+++ intunlu/generate_ensemble.py
@@ -1,12 +1,11 @@
+import logging
+
 import torch
+import numpy as np
 
 
 def token_max_score(tokens, scores):
-    return tokens
-
-
-def token_mean_score(tokens, scores):
-    return tokens
+    return tokens[scores.argmax()]
 
 
 def fill_matrix(col_len, min_score, seqs, scores):
@@ -17,7 +16,8 @@ def fill_matrix(col_len, min_score, seqs, scores):
     return seqs, scores
 
 
-def generate(models, document, device, max_input_length):
+def generate(models, document, device, max_input_length, ensemble='max'):
+    logging.info("Start generate...")
     seqs = []
     scores = []
     col_len = 0
@@ -34,10 +34,17 @@ def generate(models, document, device, max_input_length):
             return_dict_in_generate=True,
             output_scores=True
         )
-        col_len = max(col_len, len(pred[0]))
-        seqs.append(pred[0])
+        col_len = max(col_len, len(pred[0][0]))
+        seqs.append([v for v in pred[0][0]])
+        seqs.append([v for v in pred[0][0]])
 
-        scores.append([torch.max(v) for v in pred[1]])
+        # by default use max
+        if ensemble == 'mean':
+            scores.append([torch.mean(v) for v in pred[1]])
+            scores.append([torch.mean(v) for v in pred[1]])
+        else:
+            scores.append([torch.max(v) for v in pred[1]])
+            scores.append([torch.max(v) for v in pred[1]])
 
         if min_score is None:
             min_score = min(scores[-1])
@@ -45,7 +52,9 @@ def generate(models, document, device, max_input_length):
             min_score = min(min_score, min(scores[-1]) - 1.0)
     final_output = []
     seqs, scores = fill_matrix(col_len, min_score, seqs, scores)
-    for j in range(len(seqs[0])):
-        final_output.append(token_max_score(seqs[:][j], scores[:][j]))
 
-    return torch.LongTensor(final_output[0])
+    for j in range(1, col_len):
+        final_output.append(token_max_score(np.array(seqs)[:, j], np.array(scores)[:, j-1]))
+
+    logging.info("End generate...")
+    return torch.LongTensor([final_output])
